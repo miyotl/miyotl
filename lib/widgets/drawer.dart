ac60c00a29a121bfa1f7d06520a513b6ce182f3e
Gabriel Rodriguez <gabrc52@gmail.com>
2020-07-20 22:35:33 -0500

    Mostrar texto en el Drawer mientras sigue cargando

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -19,14 +19,20 @@ class AppDrawer extends StatelessWidget {
                 children: <Widget>[
                   Text(
                     app_name,
-                    style: Theme.of(context).textTheme.headline6.copyWith(color: Colors.white),
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(color: Colors.white),
                   ),
                   SizedBox(
                     height: 8,
                   ),
                   Text(
                     '${state.languageName}',
-                    style: Theme.of(context).textTheme.subtitle1.copyWith(color: Colors.white),
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle1
+                        .copyWith(color: Colors.white),
                   ),
                 ],
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -43,12 +49,15 @@ class AppDrawer extends StatelessWidget {
                 ),
               ),
             ),
-            for (var language in state.dictionaries.keys)
-              ListTile(
-                title: Text(capitalize(language)),
-                selected: language == state.language,
-                onTap: () => state.changeLanguage(language),
-              ),
+            if (state.dictionaries == null)
+              Text('Espera, sigue cargando')
+            else
+              for (var language in state.dictionaries.keys)
+                ListTile(
+                  title: Text(capitalize(language)),
+                  selected: language == state.language,
+                  onTap: () => state.changeLanguage(language),
+                ),
           ],
         ),
       ),

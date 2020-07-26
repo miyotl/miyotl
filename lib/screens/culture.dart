9c396ae9a166e80fb96570eab58e801fa26969b0
Gabriel Rodriguez <gabrc52@gmail.com>
2020-07-25 21:06:10 -0500

    Esqueleto de pantalla de cultura

--- lib/screens/culture.dart
+++ lib/screens/culture.dart
@@ -1,9 +1,41 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import '../widgets/empty_state.dart';
+import 'package:provider/provider.dart';
+import '../models/app_state.dart';
+
+class CultureCard extends StatelessWidget {
+  final CultureEntry cultureEntry;
+
+  CultureCard({this.cultureEntry});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+          children: <Widget>[
+            
+          ],
+        ),
+      elevation: 8,
+    );
+  }
+}
 
 class CulturePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return EmptyState('La función para conocer la cultura se agregará en alguna actualización futura.');
+    return Consumer<AppState>(
+      builder: (context, state, child) => GridView.count(
+        primary: true,
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        padding: EdgeInsets.all(10),
+        children: <Widget>[
+          for (CultureEntry entry in state.culture) 
+              CultureCard(cultureEntry: entry,)
+        ],
+      ),
+    );
   }
 }

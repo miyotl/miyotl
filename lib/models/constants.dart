d5f6e619e106f4c9335d97e46f9d2ff87168a857
Gabriel Rodríguez <gabrc52@gmail.com>
2021-01-08 13:52:37 -0600

    add new color (guinda)

--- lib/models/constants.dart
+++ lib/models/constants.dart
@@ -1,2 +1,3 @@
-const String dictionary_url = 'https://drive.google.com/uc?export=download&id=1A2_SIrvbM8P9vwHfju7ZYiN3twO9OTp4';
+const String dictionary_url =
+    'https://drive.google.com/uc?export=download&id=1A2_SIrvbM8P9vwHfju7ZYiN3twO9OTp4';
 const String app_name = 'Miyotl';

22f5609e678b9bde3191d679862274e5cd97e82a
Gabriel Rodríguez <gabrc52@gmail.com>
2021-01-08 13:53:06 -0600

    add new color (guinda)

--- lib/models/constants.dart
+++ lib/models/constants.dart
@@ -1,3 +1,9 @@
+import 'package:flutter/widgets.dart';
+
 const String dictionary_url =
     'https://drive.google.com/uc?export=download&id=1A2_SIrvbM8P9vwHfju7ZYiN3twO9OTp4';
 const String app_name = 'Miyotl';
+
+class AppColors {
+  static Color guinda = Color(0xFFA12042);
+}

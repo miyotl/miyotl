493ece044315c11d2de77005fb5a20d0975a930f
Gabriel <gabrc52@gmail.com>
2021-03-01 17:29:14 -0600

    add names routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
 import 'screens/onboarding.dart';
+import 'screens/developer_menu.dart';
 import 'package:provider/provider.dart';
 import 'models/app_state.dart';
 import 'models/constants.dart';
@@ -19,6 +20,10 @@ class App extends StatelessWidget {
         theme: new_light_theme,
         darkTheme: dark_theme,
         home: OnboardingPage(),
+        routes: {
+          '/onboarding': (context) => OnboardingPage(),
+          '/debug_menu': (context) => DeveloperPage(),
+        },
         debugShowCheckedModeBanner: false,
       ),
     );

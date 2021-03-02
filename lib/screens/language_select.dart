1d81b3b59fb2fc4a8f70d7b9027e54e2f474d7d9
Gabriel <gabrc52@gmail.com>
2021-03-01 22:27:38 -0600

    change language select colors

--- lib/screens/language_select.dart
+++ lib/screens/language_select.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lenguas/models/app_state.dart';
 import 'package:lenguas/widgets/status_bar_colors.dart';
 import 'package:provider/provider.dart';
@@ -17,61 +18,66 @@ class LanguageSelectPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DarkStatusBar(
-      child: Container(
-        color: AppColors.morado,
-        alignment: Alignment.topCenter,
-        child: SafeArea(
-          child: Column(
-            children: [
-              SizedBox(height: 16),
-              if (title != null)
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-                  child: Text(
-                    title,
-                    style: Theme.of(context).textTheme.headline1.copyWith(
-                          fontSize: 32,
-                          color: Colors.white,
-                        ),
+      child: Theme(
+        data: new_light_theme,
+        child: Container(
+          color: AppColors.azulMorado,
+          alignment: Alignment.topCenter,
+          child: SafeArea(
+            child: Column(
+              children: [
+                SizedBox(height: 16),
+                if (title != null)
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                    child: Text(
+                      title,
+                      style: GoogleFonts.fredokaOne(
+                        fontSize: 32,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                Text(
+                  '¿Qué idioma quieres aprender?',
+                  style: Theme.of(context).textTheme.headline2.copyWith(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                ),
+                Expanded(
+                  child: Consumer<AppState>(
+                    builder: (context, state, child) {
+                      return ListView(
+                        padding: EdgeInsets.all(8),
+                        shrinkWrap: true,
+                        children: [
+                          for (var language in state.languages)
+                            Card(
+                              // color: Colors.white,
+                              child: ListTile(
+                                title: Text('${language.name}'),
+                                subtitle:
+                                    Text('${language.speakers} hablantes'),
+                                leading: Icon(Icons.language),
+                                onTap: () {
+                                  /// Save onboarding finished and language
+                                  state.changeLanguage(language.name);
+                                  state.setOnboardingStatus(true);
+                                  state.setDefaultLanguage(state.language);
+                                  if (onLanguageSelect != null) {
+                                    onLanguageSelect(language.name);
+                                  }
+                                },
+                              ),
+                            ),
+                        ],
+                      );
+                    },
                   ),
                 ),
-              Text(
-                '¿Qué idioma quieres aprender?',
-                style: Theme.of(context).textTheme.headline2.copyWith(
-                      fontSize: 16,
-                      color: Colors.white,
-                    ),
-              ),
-              Expanded(
-                child: Consumer<AppState>(
-                  builder: (context, state, child) {
-                    return ListView(
-                      padding: EdgeInsets.all(8),
-                      shrinkWrap: true,
-                      children: [
-                        for (var language in state.languages)
-                          Card(
-                            child: ListTile(
-                              title: Text('${language.name}'),
-                              subtitle: Text('${language.speakers} hablantes'),
-                              leading: Icon(Icons.language),
-                              onTap: () {
-                                /// Save onboarding finished and language
-                                state.changeLanguage(language.name);
-                                state.setOnboardingStatus(true);
-                                state.setDefaultLanguage(state.language);
-                                if (onLanguageSelect != null) {
-                                  onLanguageSelect(language.name);
-                                }
-                              },
-                            ),
-                          ),
-                      ],
-                    );
-                  },
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),

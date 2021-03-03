c48fce2195f4779853af342756af8998f6f384ae
Gabriel <gabrc52@gmail.com>
2021-03-03 00:21:36 -0600

    animation in learn page

--- lib/screens/learn.dart
+++ lib/screens/learn.dart
@@ -4,6 +4,21 @@ import 'package:lenguas/widgets/empty_state.dart';
 class LearnPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return EmptyState('La función para aprender el idioma se agregará en alguna actualización futura.');
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'img/axolotl-gears.gif',
+            width: MediaQuery.of(context).size.width * 0.80,
+          ),
+          Text(
+            'La función para aprender el idioma se agregará en una actualización, próximamente...',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline6,
+          )
+        ],
+      ),
+    );
   }
 }

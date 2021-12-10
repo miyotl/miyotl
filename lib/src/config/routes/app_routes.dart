842a8b1bcadf0a4956594cf4723a4bfef7466b34
Jose Luis Crisostomo <jl.cs606@gmail.com>
2021-12-10 12:53:05 +0100

    update routes and theme

--- lib/src/config/routes/app_routes.dart
+++ lib/src/config/routes/app_routes.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
 class AppRoutes {
-  static Route onGenerateRoutes(RouteSettings settings) {
+  static const home = 'home';
+  static const signUp = 'signUp';
+  static const login = 'login';
+  static const culture = 'culture';
+  static const profile = 'profile';
+  static const settingsPage = 'settings';
+  static const dictionary = 'dictionary';
+  static const language = 'language';
+
+  Route onGenerateRoutes(RouteSettings settings) {
     switch (settings.name) {
-      case '/':
+      case home:
         return _materialRoute(const Scaffold());
+      case signUp:
+        return _materialRoute(const Scaffold());
+
+      case login:
+        return _materialRoute(const Scaffold());
+
+      case culture:
+        return _materialRoute(const Scaffold());
+
+      case profile:
+        return _materialRoute(const Scaffold());
+
+      case settingsPage:
+        return _materialRoute(const Scaffold());
+
       default:
         return null;
     }
--- lib/src/config/themes/app_theme.dart
+++ lib/src/config/themes/app_theme.dart
@@ -1,16 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AppTheme {
-  static ThemeData get light {
+  // From dark illustration
+  static Color darkBlue = const Color(0xFF05001E);
+  static Color pink = const Color(0xFFEC1C58);
+  static Color lightPurple = const Color(0xFFCB9FC3);
+  static Color teal = const Color(0xFF85CDC1);
+
+  // From light illustration
+  static Color lightBrown = const Color(0xFF7C6764);
+  static Color darkBrown = const Color(0xFF4C3F46);
+  static Color lightBlue = const Color(0xFFACD2DB);
+  static Color lightPink = const Color(0xFFFF7A92);
+  static Color yellow = const Color(0xFFECC730);
+
+  /// Nuevos colores
+  /* #732484 (morado)
+#ecfcac (amarillo)
+#34347c (azul-morado)
+#cc3c74 (rosa)
+#146484 (azul-verdoso)
+#occ3fb (azul claro) */
+  static Color morado = const Color(0xFF732484);
+  static Color amarillo = const Color(0xFFECFCAC);
+  static Color amarilloAjolote = const Color(0xFFFCFFAC);
+  static Color azulMorado = const Color(0xFF34347C);
+  static Color rosa = const Color(0xFFCC3C74);
+  static Color azulVerdoso = const Color(0xFF146484);
+  static Color azulClaro = const Color(0xff0cc3fb);
+  static Color azulCard = const Color(0x37334B);
+  //static Color verdePrueba = Color(0xff3BCD95);
+
+  ThemeData get light {
     return ThemeData(
       appBarTheme: const AppBarTheme(
         elevation: 0,
         color: Colors.white,
       ),
       scaffoldBackgroundColor: Colors.white,
-      primaryColor: Colors.black,
+      primaryColor: rosa,
+      secondaryHeaderColor: azulMorado,
       splashColor: Colors.transparent,
-      fontFamily: 'FredokaOne', colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.black),
+      fontFamily: 'FredokaOne',
+      colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.white),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: azulMorado,
+      backgroundColor: amarillo,
+    ),
     );
   }
+
+  // /// TODO: constant
+  // ThemeData dark_theme = ThemeData(
+  //   visualDensity: VisualDensity.adaptivePlatformDensity,
+  //   brightness: Brightness.dark,
+  //   accentColor: azulClaro,
+  //   primaryColor: lightPink,
+  //   primarySwatch: Colors.pink,
+
+  //   /// TODO: poner uno ms claro
+  //   appBarTheme: AppBarTheme(
+  //     iconTheme: IconThemeData(
+  //       color: darkBlue,
+  //     ),
+  //   ),
+  //   scaffoldBackgroundColor: darkBlue,
+  //   canvasColor: darkBlue,
+  //   cardColor: azulCard,
+  // );
+
+  // ThemeData new_light_theme = ThemeData(
+  //   visualDensity: VisualDensity.adaptivePlatformDensity,
+  //   primarySwatch: Colors.indigo,
+  //   primaryColor: rosa,
+  //   secondaryHeaderColor: azulMorado,
+  //   bottomNavigationBarTheme: BottomNavigationBarThemeData(
+  //     selectedItemColor: azulMorado,
+  //     // backgroundColor: amarillo,
+  //   ),
+
+  //   /// Workaround https://github.com/flutter/flutter/issues/72562 by manually specifying the brightness
+  //   appBarTheme: const AppBarTheme(
+  //     systemOverlayStyle: SystemUiOverlayStyle.light,
+  //   ),
+  //   selectedRowColor: rosa,
+  //   textSelectionColor: rosa,
+  // );
 }

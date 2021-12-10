d65e38d57ddb9668bf4a527fd43d4f103951d1e0
Jose Luis Crisostomo <jl.cs606@gmail.com>
2021-12-10 13:00:57 +0100

    core module finish

--- lib/src/core/utils/constants.dart
+++ lib/src/core/utils/constants.dart
@@ -8,7 +8,7 @@ const String last_update_url = 'https://proyecto-miyotl.web.app/upd.txt';
 // TODO remove later
 const String facebook_huawei_form_url =
     'https://docs.google.com/forms/u/0/d/e/1FAIpQLSfMtw1iqjFWUrec0NCmRrYOT0W_f7A3hpJIw4p3GTi04FpRWA/formResponse';
-const String app_name = 'Miyotl';
+
 const meses = <String>[
   'ene',
   'feb',
@@ -23,16 +23,8 @@ const meses = <String>[
   'nov',
   'dic'
 ];
+const String app_name = 'Miyotl';
+// API
+const String kBaseUrl = 'https://newsapi.org/v2';
 
-// ThemeData light_theme = ThemeData(
-//   visualDensity: VisualDensity.adaptivePlatformDensity,
-//   primarySwatch: Colors.teal,
-//   primaryColor: AppColors.lightBlue,
-//   accentColor: AppColors.lightPink,
-//   bottomNavigationBarTheme: BottomNavigationBarThemeData(
-//     selectedItemColor: AppColors.pink,
-//   ),
-//   selectedRowColor: AppColors.lightPink,
-//   textSelectionColor: AppColors.lightPink,
-// TextSelectionThemeData.selectionColor: AppColors.rosa,
-// );
+const String kApiKey = 'ff957763c54c44d8b00e5e082bc76cb0';

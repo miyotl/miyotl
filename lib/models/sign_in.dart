7fe09a9f1456cff48ecb5945ee5855dace84d014
Gabriel <gabrc52@gmail.com>
2022-06-08 00:43:48 -0500

    apple sign in?

--- lib/models/sign_in.dart
+++ lib/models/sign_in.dart
@@ -1,8 +1,33 @@
 // @dart=2.9
 
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:crypto/crypto.dart';
+
+/// The following methods are helpers for Apple sign-in
+/// From https://firebase.flutter.dev/docs/auth/social/#apple
+
+/// Generates a cryptographically secure random nonce, to be included in a
+/// credential request.
+String generateNonce([int length = 32]) {
+  const charset =
+      '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+  final random = Random.secure();
+  return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+      .join();
+}
+
+/// Returns the sha256 hash of [input] in hex notation.
+String sha256ofString(String input) {
+  final bytes = utf8.encode(input);
+  final digest = sha256.convert(bytes);
+  return digest.toString();
+}
 
 abstract class SignInMethods {
   /// Social sign ins methods from https://firebase.flutter.dev/docs/auth/social/
@@ -45,7 +70,42 @@ abstract class SignInMethods {
   }
 
   static Future<UserCredential> apple() async {
+    /// Reference: https://github.com/aboutyou/dart_packages/blob/master/packages/sign_in_with_apple/sign_in_with_apple/example/lib/main.dart
+    /// and https://firebase.flutter.dev/docs/auth/social/#apple
 
+    // To prevent replay attacks with the credential returned from Apple, we
+    // include a nonce in the credential request. When signing in with
+    // Firebase, the nonce in the id token returned by Apple, is expected to
+    // match the sha256 hash of `rawNonce`.
+    final rawNonce = generateNonce();
+    final nonce = sha256ofString(rawNonce);
+
+    // Request credential for the currently signed in Apple account.
+    final appleCredential = await SignInWithApple.getAppleIDCredential(
+      scopes: [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+      nonce: nonce,
+      webAuthenticationOptions: WebAuthenticationOptions(
+        clientId: 'miyotl',
+        redirectUri: Uri.parse(
+            'https://radical-foil-hortensia.glitch.me/callbacks/sign_in_with_apple'),
+      ),
+    );
+
+    // Create an `OAuthCredential` from the credential returned by Apple.
+    final oauthCredential = OAuthProvider("apple.com").credential(
+      idToken: appleCredential.identityToken,
+      rawNonce: rawNonce,
+    );
+
+    // I am supposing all the backend stuff from the package sample is unnecessary
+    // because Firebase *is* the backend
+
+    // Sign in the user with Firebase. If the nonce we generated earlier does
+    // not match the nonce in `appleCredential.identityToken`, sign in will fail.
+    return await FirebaseAuth.instance.signInWithCredential(oauthCredential);
   }
 
   static Future<UserCredential> anonymous() async {
